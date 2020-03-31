57dfdb2e01272aaf2e5a4a143657fbad28bcbd2d
David Mason <djmot@users.noreply.github.com>
2020-03-30 21:07:25 -0400

    add file browse window and thumbnail creation

--- gallery.py
+++ gallery.py
@@ -1,10 +1,14 @@
 import os
+import glob
 import PySimpleGUI as sg
 from PIL import Image
+from multiprocessing import Pool
 
+sg.theme('Dark Blue')
 FILE_EXTENSIONS = [
     '.jpg', '.jpeg', '.png',
 ]
+THUMBNAIL_SIZES = (1280, 1024, 768, 512)
 
 def list_images(folder):
     """ Return list of image filenames in folder """
@@ -20,6 +24,19 @@ def open_image(folder, image):
     im = Image.open(os.path.join(folder, image))
     im.show()
 
+def thumbnails(image):
+    """ Yield thumbnails in decreasing sizes """
+
+    im = Image.open(image)
+    for size in THUMBNAIL_SIZES:
+        im.thumbnail((size, size))
+        yield im.copy()
+
+def make_thumbnails(src, dest):
+    """ Make thumbnails from src images in the dest folder """
+
+    pass
+
 def window_event_loop(window, context):
     """ Handle window events and return result """
 
@@ -34,9 +51,8 @@ def window_event_loop(window, context):
             open_image(context['folder'], image)
 
 def show_window(folder):
-    """ Open a GUI displaying folder images """
+    """ Open a GUI to open folder images """
 
-    sg.theme('Dark Blue')
     imageTextLayout = [[
         sg.Listbox(
             values=list_images(folder), 
@@ -50,5 +66,36 @@ def show_window(folder):
     title = f'Folder: { folder }'
     window = sg.Window(title, layout)
     windowResult = window_event_loop(window, {'folder': folder})
+    print(windowResult)
+    window.close(); del window
+
+def gallery_window_event_loop(window):
+    """ Handle window events and return result """
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event is None:
+            return 'Close Window'
+        if event == 'Cancel':
+            return 'Cancel'
+        if event == 'Open':
+            folder = values['input']
+            print('Open folder:', folder)
+            show_window(folder)
+
+def show_gallery_window():
+    """ Show thumbnails in a gallery """
+
+    layout = [
+        [
+            sg.Text('Open folder:', size=(20,1)), 
+            sg.InputText(key='input', visible=False), 
+            sg.FolderBrowse(target='input'),
+        ],
+        [sg.Button('Open'), sg.Button('Cancel')]
+    ]
+    window = sg.Window('Gallery', layout)
+    windowResult = gallery_window_event_loop(window)
     print(windowResult)
     window.close(); del window
--- test.py
+++ test.py
@@ -1,22 +1,55 @@
 if __name__ == '__main__':
 
     import os
+    import shutil
     import secrets
     import random
     from gallery import *
 
-    source = secrets.galleryPath
-    galleryFolders = [
-        os.path.join(source, folder, f) 
-        for folder in os.listdir(source) 
-        for f in os.listdir(os.path.join(source, folder))
-    ]
+    def sample_gallery_folder():
+        source = secrets.galleryPath
+        for folder in os.listdir(source):
+            for f in os.listdir(os.path.join(source, folder)):
+                return os.path.join(source, folder, f)
 
-    folder = galleryFolders[0]
-    images = list_images(folder)
-    print(images[:10])
-    image = random.choice(images)
-    #open_image(folder, image)
+    def test_show_window():
+        """ Test the window (the first, super-basic one) """
 
-    show_window(folder)
-    
+        source = secrets.galleryPath
+        galleryFolders = [
+            os.path.join(source, folder, f) 
+            for folder in os.listdir(source) 
+            for f in os.listdir(os.path.join(source, folder))
+        ]
+        folder = galleryFolders[0]
+        images = list_images(folder)
+        print(images[:10])
+        image = random.choice(images)
+        #open_image(folder, image)
+        show_window(folder)
+
+    def test_gallery_window():
+        """ Test the gallery_window """
+
+        show_gallery_window()
+
+    def test_thumbnails():
+        """ Test thumbnail creation """
+
+        testDir = '.test_thumbnail'
+        src = os.path.join(testDir, 'src')
+        dest = os.path.join(testDir, 'dest')
+        try: shutil.rmtree(dest);
+        except FileNotFoundError: pass;
+        os.makedirs(dest)
+        for image in glob.glob(os.path.join(src, '*.jpeg')):
+            thumbs = thumbnails(image)
+            imageName = os.path.basename(image)
+            for size, thumb in zip(THUMBNAIL_SIZES, thumbs):
+                saveName = f'{ size }_{ imageName }'
+                thumb.save(os.path.join(dest, saveName))
+
+
+    # test_show_window()
+    # test_gallery_window()
+    test_thumbnails()
